56ebd68eff97235a9fe4fe2b9d44a78210bd77a8
Nina Persson <nina.persson83@gmail.com>
2023-02-25 17:36:33 +0100

    Created a new test

--- test_tui.py
+++ test_tui.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, main
 from unittest.mock import patch
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,8 +15,8 @@ class TestTUI(TestCase):
     # A customer can reach the checkout page
     """
 
-    # Helper for waiting explicitly                                           Kolla vad som krävs för explicit waits!!
-    def document_initialised(self.driver):
+    # Helper for waiting explicitly     Kolla vad som krävs för explicit waits i Unittest!!
+    def document_initialised(self):
         return self.driver.execute_script("return initialised")
 
     def setUp(self):
@@ -29,7 +29,6 @@ class TestTUI(TestCase):
         self.assertIn("tui", self.driver.current_url) # Måste man definiera current_url nånstans eller fattar Python det själv? (Kolla Eriks inspelningar igen!)
 
     def test_main_shop_page(self):
-        
         # Find Products page link
         products_link = self.driver.find_element_by_XPATH, "Products")
 
@@ -37,7 +36,7 @@ class TestTUI(TestCase):
         products_link.click()
 
         # Including explicit wait
-        scar_original = WebDriverWait(self.driver, timeout = 10).until(lambda d: d.find_element_by_XPATH, "/html/body/div/main/section/div/main/a[1]/div/div[2]/h2"))
+        scar_original = WebDriverWait(self.driver, timeout = 10).until(lambda d: d.find_element_by_XPATH, "/html/body/div/main/section/div/main/a[1]/div/div[2]/h2")
 
         # Test the dropdown "Resor" and verify that it contains a title "Charterresor"
         self.assertIn(a, b)
@@ -62,7 +61,7 @@ class TestTUI(TestCase):
          
                  
         # Enter "Avreseort"
-        self.avreseort = self.driver.find_element_by_XPATH(/html/body/div[1]/div/section/section[1]/div/div/div/div/div[2]/div[1]/div/div/div[1]/div/div[2]/div/input)
+        self.avreseort = self.driver.find_element_by_XPATH("/html/body/div[1]/div/section/section[1]/div/div/div/div/div[2]/div[1]/div/div/div[1]/div/div[2]/div/input")
         # Enter "Resmål"
             
         # Enter date
@@ -83,18 +82,32 @@ class TestTUI(TestCase):
         # Verify that you are now in checkout page
         self.assertIn("payment", self.driver.current_url)
 
-    def test_chat(self): # Hittat på ett annat test eftersom man testar både adding to cart/cart page och checkout page samtidigt
-        # Kundservice/Chatt
-
-        # Fyll i ditt namn
-
-        # Klicka på skicka
-
-        # Kommer en knapp upp som heter "Jag har en bokning"?
+    def test_chat(self): # Hittat på ett annat test eftersom man testar både adding to cart, cart page och checkout page samtidigt
+        # Find Kundservice link
+        self.customer_service = self.driver.find_element_by_XPATH("/html/body/div[1]/div/header/div/div/div/div/div/ul/li[2]/span/a")
+        # Click the link
+        self.customer_service.click()
+        # Find chat
+        self.chat = self.driver.find_element_by_XPATH("/html/body/div[4]/button/span")
+        # Click chat
+        self.chat.click()
+        # Find input field to answer chatbot
+        self.input_field = self.driver.find_element_by_XPATH("/html/body/div[1]/div/div/form/div/input")
+        # Clear text
+        self.input_field.clear()
+        # Input your name
+        self.input_field.send_keys("Nina Persson")
+        # Find Skicka-button
+        self.send = self.driver.find_element_by_XPATH("/html/body/div[1]/div/div/form/button")
+        # Click button
+        self.send.click()
+        # Find "Jag har en fråga" button and make an assertion that it is there
+        self.button = self.driver.find_element_by_NAME("Jag har en fråga") 
+        self.assertIsNotNone(self.button)
 
     def tearDown(self):
         self.driver.delete_all_cookies()     
         self.driver.quit()  
 
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        main()
